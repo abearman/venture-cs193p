48e07a216b10c2396954366d88c36d1bf2634e58
amybear <amy@hyperfine.com>
2014-06-05 15:41:42 -0700

    Added timestamp to Message

--- Venture1/Message.h
+++ Venture1/Message.h
@@ -1,18 +1,20 @@
 //
 //  Message.h
-//  Venture
+//  Venture1
 //
-//  Created by Amy Bearman on 5/25/14.
+//  Created by Amy Bearman on 6/5/14.
 //  Copyright (c) 2014 Amy Bearman. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
+@class Person;
 
 @interface Message : NSManagedObject
 
 @property (nonatomic, retain) NSString * message;
-@property (nonatomic, retain) NSManagedObject *sender;
+@property (nonatomic, retain) NSDate * timestamp;
+@property (nonatomic, retain) Person *sender;
 
 @end
--- Venture1/Message.m
+++ Venture1/Message.m
@@ -1,17 +1,19 @@
 //
 //  Message.m
-//  Venture
+//  Venture1
 //
-//  Created by Amy Bearman on 5/25/14.
+//  Created by Amy Bearman on 6/5/14.
 //  Copyright (c) 2014 Amy Bearman. All rights reserved.
 //
 
 #import "Message.h"
+#import "Person.h"
 
 
 @implementation Message
 
 @dynamic message;
+@dynamic timestamp;
 @dynamic sender;
 
 @end
